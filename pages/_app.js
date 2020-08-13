fdec7f4cd7aae03d8e3b7e57b54315545b5ef762
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-08-13 23:31:01 +0530

    refactor: adds mock on all environments

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-if (process.env.NODE_ENV === "development") {
-  require("mocks");
-}
+// For demo purposes, we're loading the mock data from MSW. In a real app, the
+// mocks should be present only for 'development' environment. The code for
+// that change is below.
+// if (process.env.NODE_ENV === "development") {
+//   require("mocks");
+// }
+
+// If you want to include the mocks only on 'development' environment, you
+// should remove the next line and uncomment lines 4, 5 and 6.
+require("mocks");
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
